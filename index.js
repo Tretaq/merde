154422f10be955a9c7870d21bc3e12d6f4bb1c34
Tretaq <jakub.jamrozyy@gmail.com>
2026-06-24 16:06:33 +0200

    added variable adding from the command to /merde-search-monster

--- index.js
+++ index.js
@@ -13,18 +13,34 @@ app.command("/merde-ping", async ({ command, ack, respond }) => {
   const start = Date.now();
   await ack();
   const latency = Date.now() - start;
+  
   await respond({ text: `Pong!\nLatency: ${latency}ms` });
 });
 
-app.command("/merde-catfact", async ({ ack, respond }) => {
-  await ack();
+// app.command("/merde-catfact", async ({ ack, respond }) => {
+//   await ack();
 
-  try {
-    const response = await axios.get("https://catfact.ninja/fact");
-    await respond({ text: `Cat Fact:\n${response.data.fact}` });
-  } catch (err) {
-    await respond({ text: "Failed to fetch a cat fact." });
+//   try {
+//     const response = await axios.get("https://catfact.ninja/fact");
+//     await respond({ text: `Cat Fact:\n${response.data.fact}` });
+//   } catch (err) {
+//     await respond({ text: "Failed to fetch a cat fact." });
+//   }
+// });
+
+app.command("/merde-search-monster", async({ command , ack , respond}) => {
+  await ack();
+  
+  
+  const argument = command.text.trim();
+  console.log(argument);
+  if(!argument){
+    await respond('Please provide a topic! e.g. `/merde-fact owlbear`');
+    return;
   }
+  await respond({ text: `Written ${argument}` });
+
+
 });
 
 app.command("/merde-help", async ({ ack, respond }) => {
